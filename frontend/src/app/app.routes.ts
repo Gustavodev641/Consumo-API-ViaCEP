3e1c01acd801fd1f0346609dd1e7f5590862bb52
Gustavo Nascimento <gustavopdn1@gmail.com>
2026-04-16 23:01:04 -0300

    feat: tela de lista de usuarios

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { ListaComponent } from './pages/usuarios/lista/lista.component';
+import { FormularioComponent } from './pages/usuarios/formulario/formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'usuarios', pathMatch: 'full' },
+  { path: 'usuarios', component: ListaComponent },
+  { path: 'usuarios/novo', component: FormularioComponent },
+  { path: 'usuarios/:id/editar', component: FormularioComponent }
+];
