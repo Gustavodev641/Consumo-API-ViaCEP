7ab10efcabde6d564e73d86dd43ae4773910d413
Gustavo Nascimento <gustavopdn1@gmail.com>
2026-04-20 13:48:49 -0300

    fix: add de rotas

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -2,6 +2,14 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'usuarios/:id/editar',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'usuarios/:id',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
